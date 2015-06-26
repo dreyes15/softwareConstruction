47e499eeff7403411543167c47b39d0e75ac0760
Lucia <lvrod_000@LuciaRodriguez>
2015-06-26 17:56:35 -0600

    AfterL

--- SoftwareConstruction/AfterL.java
+++ SoftwareConstruction/AfterL.java
@@ -1,4 +1,12 @@
-
+/*
+ * This class is a type of scope that will describe the extent of 
+ * program execution over which property holds. 
+ * 
+ * With the given information this class should be able to generate itself
+ * as the "After L" scope. 
+ * 
+ * After L will denote the execution after the first time L holds.
+ */
 public class AfterL {
 
 }
