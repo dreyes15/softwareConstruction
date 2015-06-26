a52d00a5107d60b25f4f86e4ccb5da9f02a4cc8d
Troy McGarity <tamcgarity@miners.utep.edu>
2015-06-26 15:59:06 -0600

    Class descriptions for LTLGenerator, Property, and FormulaCreator

--- SoftwareConstruction/src/Property.java
+++
@@ -1,8 +0,0 @@
-/*This class is the main driver of the Input Definition subsystem.  It receives
- * a string array containing a Scope, a Pattern, and all necessary Proposition types
- * (including the number of each proposition for composite types), and it returns an
- * Property object that contains this information.
- */
-public class Property {
-
-}
