f378e5f08c51f7825a16320491cf29a82084bb44
Troy McGarity <tamcgarity@miners.utep.edu>
2015-06-25 23:00:12 -0600

    Entered some initial information for LTLGenerator, Property, and FormulaCreator.

---
+++ SoftwareConstruction/src/FormulaCreator.java
@@ -0,0 +1,6 @@
+/*This class is the main driver of the Output Generation subsystem.  It receives
+ * a Property object and it returns an LTL Formula that matches the given Property data.
+ */
+public class FormulaCreator {
+
+}
