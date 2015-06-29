16eb9eaaa4200322784243443382f6514afd7997
David Reyes <davidreyes@Davids-MacBook-Pro-3.local>
2015-06-29 17:45:54 -0400

    Changes made as per feedback discussed today

--- SoftwareConstruction/Precedence.java
+++ SoftwareConstruction/Precedence.java
@@ -1,10 +1,10 @@
 /* This class defines the pattern that, if in the portion of the system, an occurence of the secondary
  * propsiton (q) is a necessary pre-condition of an ocurence of p.this class will do this by using the method
- * getPrecendence().
+ * Precendence().
  */
-public class Precedence {
+public class Precedence extends Pattern{
     
-    /* Returns the name of the pattern (Precedence) to be futher used by the LTL generator.
+    /* Creates an object of type Precedence to be futher used by the LTL generator.
      */
 
 
