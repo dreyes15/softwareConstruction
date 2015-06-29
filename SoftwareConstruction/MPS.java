fd324ede9f63d9c1e939cc1f5f162fb09e65a931
Troy McGarity <tamcgarity@miners.utep.edu>
2015-06-29 16:24:15 -0600

    Updated descriptions of these classes.

---
+++ SoftwareConstruction/MPS.java
@@ -0,0 +1,12 @@
+/*
+ * The purpose of this class is to generate a listing of the Maximal Propositional 
+ * Subformulas from the whole formula. It will have one method called createMPSArray().
+ */
+public class MPS {
+    
+    /*
+     * createMPSArray() will receive a modified formula string from the SpecialOperator
+     * class.  It will parse this string into individual MPSs and special operators into
+     * a string array and return this array to the SpecialOperator class.
+     */
+}
