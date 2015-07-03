b2df0c1d696d7a4ff004852efe1106e0dc878d6d
framos2 <fl1109@live.com>
2015-07-02 19:59:54 -0600

    Renamed class LTLSubstitutor to CPSubstitutor. Added implementation to CPSubstitutor class.

--- SoftwareConstruction/CPSubstitutor.java
+++ SoftwareConstruction/CPSubstitutor.java
@@ -2,8 +2,8 @@
  * The purpose of this class is to substitute the proposition placeholders with their
  * respective composite forms.  It will have one method called substituteCompositeProps().
  */
-public class LTLSubstitutor {
-    
+public class CPSubstitutor {
+
     /*
      * substituteCompositeProps() will receive a base formula, Scope, and Pattern from 
      * SubstitutionTable as parameters.  It will search the string for the proposition
@@ -13,6 +13,19 @@ public class LTLSubstitutor {
      * SpecialOperator class.  It will receive a final formula string in return from
      * SpecialOperator, and it will return this final formula string to SubstitutionTable.
      */
-    
-    
+    substituteCompositeProps(String baseFormula, Proposition propP, Proposition propQ, Proposition propL, Proposition propR){
+    	//Generate the CPs of the placeholders
+    	String CPofP = propP.generateCP();
+    	String CPofQ = propQ.generateCP();
+       	String CPofL = propL.generateCP();
+    	String CPofR = propR.generateCP();
+    	
+    	//Replace the proposition placeholders with the appropriate CP type formula
+    	String baseWithP = baseFormula.replaceAll("P", CPofP)
+    	String baseWithPQ = baseWithP.replaceAll("Q", CPofQ);
+    	String baseWithPQR = baseWithPQ.replaceAll("R", CPofR);
+    	String baseWithPQRL = baseWithPQR.replaceAll("L", CPofL);
+    	
+    	//Call to the SubstitutionTable
+    }
 }
