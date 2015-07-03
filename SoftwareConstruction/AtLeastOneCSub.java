43f15e0cd936d424488e89bfcf6ebf0d1463d784
framos2 <fl1109@live.com>
2015-07-02 22:23:31 -0600

    Added Sub classes for AtLeastOne and Parallel

---
+++ SoftwareConstruction/AtLeastOneCSub.java
@@ -0,0 +1,9 @@
+/* This class defines an occurrence the primary proposition (p) does not occur
+ * this class will do this by using the method Absence().
+ */
+public class Absence  extends Pattern {
+    
+    /* Creates an object of type Absence to be further used by the LTL generator.
+     */
+
+}
