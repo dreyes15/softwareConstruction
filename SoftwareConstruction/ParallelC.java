ec88f8bb28eed28e64c8fab2e2c7f988e9f35f10
Victoria <vsbravo@miners.utep.edu>
2015-07-02 20:39:25 -0600

    Parallel methods for C, E, and H added

--- SoftwareConstruction/ParallelC.java
+++ SoftwareConstruction/ParallelC.java
@@ -3,14 +3,31 @@
  */
 
 public class ParallelC extends Composite {
-	
-	/* ParallelC(String name, int number) is a constructor method for a composite
-	 * proposition of type ParallelC. The name being the name of the proposition
-	 * and the number is the number of propositions that make up this composite
-	 * proposition.  
+
+	/*
+	 * ParallelC(String name, int number) is a constructor method for a
+	 * composite proposition of type ParallelC. The name being the name of the
+	 * proposition and the number is the number of propositions that make up
+	 * this composite proposition.
 	 */
 	public ParallelC(String name, int number){
 		super(name, number);
 		typeOfProposition = "ParallelC";
+		
+		public static String ParallelCSubstitution(String name, int number) {
+
+			String sub = " ";
+
+			for (int i = 1; i <= number; i++) {
+				while(i != number){
+				sub += name + i + " ^ ";
+				i++;
+				}
+				if(i == number){
+					sub += name + i;
+				}
+			}
+			return sub;
+		}
 	}
 }
--- SoftwareConstruction/ParallelH.java
+++ SoftwareConstruction/ParallelH.java
@@ -1,4 +1,32 @@
+/* Represents a composite proposition of type hold, that holds at a state 's' if all of
+ * the propositions are true at some state 's'.   
+ */
 
-public class ParallelH {
+public class ParallelH extends Composite {
+	
+	/* ParallelH(String name, int number) is a constructor method for a composite
+	 * proposition of type ParallelH. The name being the name of the proposition
+	 * and the number is the number of propositions that make up this composite
+	 * proposition.  
+	 */
+	public ParallelH(String name, int number){
+		super(name, number);
+		typeOfProposition = "ParallelH";
+	}
+	public static String ParallelHSubstitution(String name, int number) {
 
+		String sub = " ";
+
+		for (int i = 1; i <= number; i++) {
+			while(i != number){
+			sub += name + i + " ^ ";
+			i++;
+			}
+			if(i == number){
+				sub += name + i;
+			}
+		}
+		return sub;
+	}
+ 
 }
