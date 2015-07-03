c40eb424f14c6023887f3237d65654e9bdfb4361
David Reyes <davidreyes@Davids-MacBook-Pro.local>
2015-07-02 23:06:19 -0400

    no message

--- SoftwareConstruction/EventualC.java
+++ SoftwareConstruction/EventualC.java
@@ -15,4 +15,45 @@ public class EventualC extends Composite {
 		super(name, number);
 		typeOfProposition = "EventualC";
 	}
+    
+    public static String EventualE(String property, int number){
+        //baseProposition (pn-1 ^X(!pn U pn))
+        String finalProposition = "";
+        
+        if(number == 1){
+            finalProposition = endPropostion(property,number);
+            return finalProposition;
+        }
+        
+        int i= 1;
+        while(i <= number){
+            
+            if(i == 1) {
+                finalProposition = baseProposition(property,i);
+            }
+            
+            else if (i< number){
+                finalProposition = finalProposition.replace(".", midProposition(property, i));
+            }
+            else{
+                finalProposition = finalProposition.replace(".", endPropostion(property, i));
+            }
+            i++;
+        }
+        return finalProposition;
+    }
+    
+    //Creates the general (pn ^X(.)) where the . will be replaced
+    private static String baseProposition(String property, int number){
+        return "( " + property+number+ " ^X .)";
+    }
+    
+    private static String midProposition(String property, int number){
+        return "(!"+property+number+"U("+property+number+"^X.)";
+    }
+    
+    private static String endPropostion(String property, int number){
+        return "(!" +property+number+" U "+property+number+")";
+    }
+    
 }
