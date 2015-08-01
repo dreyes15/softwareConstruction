1b8d21681b60435227c64098e470b9920ac5cb91
framos2 <fl1109@live.com>
2015-07-31 18:25:14 -0600

    Changed brackets in FormulaCreator class (still needs internal errors to be fixed, but now it recognizes the class and methods). Edited comments in the classes.

--- SoftwareConstruction/FormulaCreator.java
+++ SoftwareConstruction/FormulaCreator.java
@@ -1,9 +1,9 @@
-/*This class is the main driver of the Output Generation subsystem.  It receives
+/* This class is the main driver of the Output Generation subsystem.  It receives
  * a Property object and it returns an LTL Formula that matches the given Property data.
- This class contains one method called createFormula()*/
+ * This class contains one method called createFormula()
+ */
 
 public class FormulaCreator {
-
 	/*createFormula() receives a Property object from LTLGenerator.  It passes the Scope
 	 * and Pattern objects to a method it calls from the SubstitionTable class and returns
 	 * a FinalFormula string to LTLGenerator.  
@@ -20,7 +20,7 @@ public class FormulaCreator {
 		String localPattern = pattern.getPatternType();
 
 		//Passes the scope and pattern to SubstitutionTable and gets a baseFormula in return
-		String baseFormula = getTable(scope, pattern);
+		String baseFormula = SubstitutionTable.getTable(scope, pattern);
 
 		//Passes the baseFormulal to specialOperationsReplacement to get an updatedBaseFormula
 		String updatedBaseFormula = specialOperationsReplacement(basedFormula);
@@ -28,41 +28,18 @@ public class FormulaCreator {
 		//Performs proposition substitution on the updatedBaseFormula conditioned on the scope and pattern 
 		//types to obtain a modifiedFormula
 
-		public void 
-		//If the scope is Global 
-		
-		if(localScope.equals("Global")){
-			//And the pattern is Absence or Existence
-			if(localPattern.equals("Absence") || localPattern.equals("Existence")){
-				//get the Prop p
-				Proposition propP = pattern.getPropositionP();
-				//Performs propositions substitution on propP
-				String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
-				
-			}
-			//Otherwise
-			else{
-				//Performs propositions substitution on propP and propQ
-				String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
-				modifiedFormula = propSubstitutor.substituteCompositeProps(modifiedFormula, propQ);
-			}
-		}
-		
-		//if the scope is Global
-		public String getModifiedFormula(Global scope, Absense Pattern){
+		public void m1() {
+			//If the scope is Global 
+
+			if(localScope.equals("Global")){
+				//And the pattern is Absence or Existence
+				if(localPattern.equals("Absence") || localPattern.equals("Existence")){
+					//get the Prop p
 					Proposition propP = pattern.getPropositionP();
 					//Performs propositions substitution on propP
 					String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
-					return modifiedFormula;
+
 				}
-		public String getModifiedFormula(Global scope, Existence Pattern){
-			Proposition propP = pattern.getPropositionP();
-			//Performs propositions substitution on propP
-			String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
-			return modifiedFormula;
-			
-		}
-		
 				//Otherwise
 				else{
 					//Performs propositions substitution on propP and propQ
@@ -70,14 +47,29 @@ public class FormulaCreator {
 					modifiedFormula = propSubstitutor.substituteCompositeProps(modifiedFormula, propQ);
 				}
 			}
-			
+
+			//if the scope is Global
+			public String getModifiedFormula(Global scope, Absense Pattern) {
+				Proposition propP = pattern.getPropositionP();
+				//Performs propositions substitution on propP
+				String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
+				return modifiedFormula;
+			}
+			public String getModifiedFormula(Global scope, Existence Pattern) {
+				Proposition propP = pattern.getPropositionP();
+				//Performs propositions substitution on propP
+				String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
+				return modifiedFormula;
+
+			}
+
+			//Otherwise
+			else{
+				//Performs propositions substitution on propP and propQ
+				String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
+				modifiedFormula = propSubstitutor.substituteCompositeProps(modifiedFormula, propQ);
+			}
 		}
-		
-		
-		
-		
-		
-		
 
 		//If the scope is BeforeR
 		else if(localScope.equals("BeforeR")){
@@ -86,7 +78,7 @@ public class FormulaCreator {
 				//Performs propositions substitution on propP and propR
 				String modifiedFormula = propSubstitutor.substituteCompositeProps(updatedBasedFormula, propP);
 				modifiedFormula = propSubstitutor.substituteCompositeProps(modifiedFormula, propR);
-				
+
 			}
 			//Otherwise
 			else{
@@ -140,5 +132,4 @@ public class FormulaCreator {
 		//Returns the finalFormula to LTLGenerator
 		return finalFormula;
 	}
-
 }
--- SoftwareConstruction/Proposition.java
+++ SoftwareConstruction/Proposition.java
@@ -4,7 +4,8 @@
  * Project: LTL Generator
  * Sprint: CP Generation
  * Due Date: July 3, 2015
- * Purpose: Represents a proposition variable in propositional logic, where propositions
+ * Purpose:
+ * Represents a proposition variable in propositional logic, where propositions
  * are declarative sentences. This class is abstract and cannot be instantiated. A 
  * Proposition will have a name, and be either an atomic or composite proposition. If 
  * the Proposition is composite, it will also have a composite proposition (CP) class. 
