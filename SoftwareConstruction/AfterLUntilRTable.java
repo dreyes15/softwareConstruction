8bd2d5905cb49206e47b5ef6a749b44f68ccac86
framos2 <fl1109@live.com>
2015-08-02 11:50:48 -0600

    Changed conditional to a method in AfterLuntilRTable class

--- SoftwareConstruction/AfterLUntilRTable.java
+++ SoftwareConstruction/AfterLUntilRTable.java
@@ -36,15 +36,15 @@ public class AfterLUntilRTable {
         String rSubFormula = " ";
         String gSubFormula = " ";
         
-        char scopeLetter = getLastLetter(R);
+        char rLastLetter = getLastLetter(R);
         
         gSubFormula = GlobalTable.getFormula(pattern);
         rSubFormula = BeforeRTable.getFormula(pattern, R);
         
-        if(scopeLetter == 'C' || scopeLetter == 'c'){
+        if(isC(rLastLetter)){
             baseFormula = "[]((L&r!R)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
-        else if(scopeLetter == 'E' || scopeLetter == 'e'){
+        else if(isE(rLastLetter)){
             baseFormula = "[]((L)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
         return baseFormula;
@@ -57,15 +57,15 @@ public class AfterLUntilRTable {
         String rSubFormula = "";
         String gSubFormula = "";
         
-        char scopeLetter = getLastLetter(R);
+        char rLastLetter = getLastLetter(R);
         
         gSubFormula = GlobalTable.getFormula(pattern);
         rSubFormula = BeforeRTable.getFormula(pattern, R);
         
-        if(scopeLetter == 'C' || scopeLetter == 'c'){
+        if(isC(rLastLetter)){
             baseFormula = "[]((L&r!R)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
-        else if(scopeLetter == 'E' || scopeLetter == 'e'){
+        else if(isE(rLastLetter)){
             baseFormula = "[]((L)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
         return baseFormula;
@@ -77,15 +77,15 @@ public class AfterLUntilRTable {
         String rSubFormula = "";
         String gSubFormula = "";
         
-        char scopeLetter = getLastLetter(R);
+        char rLastLetter = getLastLetter(R);
         
         gSubFormula = GlobalTable.getFormula(pattern);
         rSubFormula = BeforeRTable.getFormula(pattern, R);
         
-        if(scopeLetter == 'C' || scopeLetter == 'c'){
+        if(isC(rLastLetter)){
             baseFormula = "[]((L&r!R)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
-        else if(scopeLetter == 'E' || scopeLetter == 'e'){
+        else if(isE(rLastLetter)){
             baseFormula = "[]((L)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
         return baseFormula;
@@ -97,15 +97,15 @@ public class AfterLUntilRTable {
         String rSubFormula = "";
         String gSubFormula = "";
         
-        char scopeLetter = getLastLetter(R);
+        char rLastLetter = getLastLetter(R);
         
         gSubFormula = GlobalTable.getFormula(pattern);
         rSubFormula = BeforeRTable.getFormula(pattern, R);
         
-        if(scopeLetter == 'C' || scopeLetter == 'c'){
+        if(isC(rLastLetter)){
             baseFormula = "[]((L&r!R)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
-        else if(scopeLetter == 'E' || scopeLetter == 'e'){
+        else if(isE(rLastLetter)){
             baseFormula = "[]((L)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
         return baseFormula;
@@ -117,18 +117,32 @@ public class AfterLUntilRTable {
         String rSubFormula = "";
         String gSubFormula = "";
         
-        char scopeLetter = getLastLetter(R);
+        char rLastLetter = getLastLetter(R);
         
         gSubFormula = GlobalTable.getFormula(pattern);
         rSubFormula = BeforeRTable.getFormula(pattern, R);
         
-        if(scopeLetter == 'C' || scopeLetter == 'c'){
+        if(isC(rLastLetter)){
             baseFormula = "[]((L&r!R)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
-        else if(scopeLetter == 'E' || scopeLetter == 'e'){
+        else if(isE(rLastLetter)){
             baseFormula = "[]((L)->(L&l((" + rSubFormula+ "^((!<>R)->"+gSubFormula+")))))";
         }
         return baseFormula;
+    }
+    
+    private static boolean isC(char endLetter) {
+    	if (endLetter == 'C' || endLetter == 'c'){
+    		return true;
+    	}
+    	return false;
+    }
+    
+    private static boolean isE(char endLetter) {
+    	if (endLetter == 'E'){
+    		return true;
+    	}
+    	return false;
     }
     
     private static char getLastLetter(Proposition prop){
