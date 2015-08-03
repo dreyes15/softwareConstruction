e62c57d42724e651b5319028d699d8c16f944e82
Troy McGarity <tamcgarity@miners.utep.edu>
2015-08-02 22:03:10 -0600

    Reverted AndL and AndR back to original form.

--- SoftwareConstruction/ltlGenerator/formulaBuilder/specialOperator/AndL.java
+++ SoftwareConstruction/ltlGenerator/formulaBuilder/specialOperator/AndL.java
@@ -1,4 +1,7 @@
 package ltlGenerator.formulaBuilder.specialOperator;
+
+import ltlGenerator.formulaBuilder.specialOperator.FormulaSplicer;
+
 /* 
  * The purpose of this class is to add information to the last state of the LTL Formula before the
  * &l special operator.
@@ -14,43 +17,35 @@ public class AndL {
 
 	private static final String END_PARENTHESIS = "\\)";
 
-	public String replaceAndL(String modifiedFormula) {
-
+public String replaceAndL(String modifiedFormula) {
+		
 		FormulaSplicer splicer = new FormulaSplicer();
 		String andLModifiedFormula = modifiedFormula;
-		int searchPosition = 0;
-
+		int searchPosition = andLModifiedFormula.length() - 1;
+		
 		//Starts searching from the end of the formula to the beginning of the formula
-		while (searchPosition < andLModifiedFormula.length()-2) {
-			
+		while (searchPosition > 0) {
 			//Searches for the &l operator
-			if (andLModifiedFormula.charAt(searchPosition) == '&' && andLModifiedFormula.charAt(searchPosition + 1) == 'l')  {
-				
-				//Gets the portion of the formula after the &l operator
-				String subFormulaToAdd = splicer.getSubformulaToAdd(andLModifiedFormula, searchPosition + 1);
-				
-				//Removes the &r operator and the subformula it is splicing into the portion before the &r
-				andLModifiedFormula = splicer.removeSubformula(andLModifiedFormula, searchPosition, searchPosition + 1 + subFormulaToAdd.length());
-				searchPosition++;
-				
-				//Finds the beginning of the last set of close parentheses before the position of the &l operator
-				int addPosition = getPositionToAddSubformula(andLModifiedFormula, searchPosition);
-
-				//Adds the splice information to the location found in the previous step
-				andLModifiedFormula = splicer.addSubformula(andLModifiedFormula, "^" + subFormulaToAdd, addPosition + 1);
+			if (andLModifiedFormula.charAt(searchPosition) == 'l') {
+				searchPosition--;
+				if (andLModifiedFormula.charAt(searchPosition) == '&') {
+					//Gets the portion of the formula after the &l operator
+					String subFormulaToAdd = splicer.getSubformulaToAdd(andLModifiedFormula, searchPosition + 1);
+					//Removes the &r operator and the subformula it is splicing into the portion before the &r
+					andLModifiedFormula = splicer.removeSubformula(andLModifiedFormula, searchPosition, searchPosition + 1 + subFormulaToAdd.length());
+					searchPosition--;
+					//Finds the beginning of the last set of close parentheses before the position of the &l operator
+					while (andLModifiedFormula.substring(searchPosition, searchPosition + 1).matches(END_PARENTHESIS)) {
+						searchPosition--;
+					}
+					//Adds the splice information to the location found in the previous step
+					andLModifiedFormula = splicer.addSubformula(andLModifiedFormula, "^" + subFormulaToAdd,searchPosition + 1);
+				}
 			}
 			//Continues searching for &l operators until it reaches the beginning of the formula string
-			searchPosition++;
+			searchPosition--;
 		}
 		//returns the formula with splices inserted in accordance with all &l operators contained in original formula
 		return andLModifiedFormula;
-	}
-
-	private static int getPositionToAddSubformula(String formula, int searchPosition) {
-		int positionFound = searchPosition;
-		while (formula.substring(positionFound, positionFound + 1).matches(END_PARENTHESIS)) {
-			positionFound--;
-		}
-		return positionFound;
 	}
 }
