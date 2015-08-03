7278a7a374308288c4dd3c0181f34a6a8b0cda54
Troy McGarity <tamcgarity@miners.utep.edu>
2015-08-03 14:43:58 -0600

    Re-factored AndR class by pulling out separate methods for AndR and AndE

--- SoftwareConstruction/ltlGenerator/formulaBuilder/specialOperator/AndR.java
+++ SoftwareConstruction/ltlGenerator/formulaBuilder/specialOperator/AndR.java
@@ -10,18 +10,19 @@ package ltlGenerator.formulaBuilder.specialOperator;
  */
 public class AndR{
 	
+	FormulaSplicer splicer = new FormulaSplicer();
 	private static final String END_PARENTHESIS = "\\)";
+	//String subformulaToAdd = "";
 	
 	/*
-     * The replaceAndR() method will receive a formula and search it for any &r operators.
-     * It will determine the portion of the formula after the &r operator that applies to it, remove
+     * The replaceAndR() method will receive a formula and search it for any &r or &e operators.
+     * It will determine the portion of the formula after the &r/&e operator that applies to it, remove
      * this portion from the formula, and add it to the each section(state) of the original formula to
-     * which the special operator pertains. Once all of the &r operations have been performed, it will
+     * which the special operator pertains. Once all of the &r/&e operations have been performed, it will
      * return the resulting andRModifiedFormula.
      */
 	public String replaceAndR(String formula){
 
-		FormulaSplicer splicer = new FormulaSplicer();
 		String andRModifiedFormula = formula;
 		int searchPosition = andRModifiedFormula.length() - 1;
 		
@@ -34,80 +35,86 @@ public class AndR{
 				//Check for &r operators of a Condition type
 				if (andRModifiedFormula.charAt(searchPosition + 1) == 'r'){
 					
-					//Gets the portion of the formula after the &r operator
-					String subFormulaToAdd = splicer.getSubformulaToAdd(andRModifiedFormula, searchPosition + 1);
-					
-					//Removes the &r operator and the subformula it is splicing into the portion before the &r
-					andRModifiedFormula = splicer.removeSubformula(andRModifiedFormula, searchPosition, searchPosition + 1 + subFormulaToAdd.length());
-					
-					//Gets the beginning position of the portion of the formula that needs to be spliced into
-					int startPosition = splicer.getBeginSubformulaToSplicePosition(andRModifiedFormula, searchPosition);
-					
-					//Searches the portion of formula that needs to be spliced into for the places the splice needs to be added
-					while (startPosition < searchPosition){
-						startPosition++;
-						
-						//Finds all the sets of close parentheses
-						if (andRModifiedFormula.substring(startPosition,startPosition+1).matches(END_PARENTHESIS) && !andRModifiedFormula.substring(startPosition-1,startPosition).matches("\\)")){
-							
-							//Adds the splice in front of these sets
-							andRModifiedFormula = splicer.addSubformula(andRModifiedFormula, "^" + subFormulaToAdd, startPosition);
-							
-							//Normalizes the search and start positions after the subformula is added
-							searchPosition += subFormulaToAdd.length();							
-							startPosition += subFormulaToAdd.length()+1;
-						}
-						
-						//Finds all the "next" operators
-						else if (andRModifiedFormula.substring(startPosition,startPosition+1).matches("X")){
-							
-							//Adds the splice in front of these operators
-							andRModifiedFormula = splicer.addSubformula(andRModifiedFormula, subFormulaToAdd + "^", startPosition);
-							
-							//Normalizes the search and start positions after the subformula is added
-							searchPosition += subFormulaToAdd.length();	
-							startPosition += subFormulaToAdd.length()+1;
-						}
-					}
+					//Perform the AndR operation at this location in the andRModifiedFormula string
+					andRModifiedFormula = performAndROperation(andRModifiedFormula, searchPosition);	
 				}
 				
 				//Check for &r operators of an Event type
 				else if (andRModifiedFormula.charAt(searchPosition + 1) == 'e'){
 					
-					//Gets the portion of the formula after the &e operator
-					String subformulaToAdd = splicer.getSubformulaToAdd(andRModifiedFormula, searchPosition + 1);
-					
-					//Removes the &e operator and the subformula it is splicing into the portion before the &e
-					andRModifiedFormula = splicer.removeSubformula(andRModifiedFormula, searchPosition, searchPosition + 1 + subformulaToAdd.length());
-					
-					//Gets the beginning position of the portion of the formula that needs to be spliced into
-					int startPosition = splicer.getBeginSubformulaToSplicePosition(andRModifiedFormula, searchPosition);
-					
-					//Searches the portion of formula that needs to be spliced into for the places the splice needs to be added
-					while (searchPosition > startPosition+1){
-						searchPosition--;
-						
-						//Finds all the sets of close parentheses
-						if (andRModifiedFormula.substring(searchPosition-1,searchPosition).matches(END_PARENTHESIS)){
-							searchPosition--;
-							if (andRModifiedFormula.substring(searchPosition-1,searchPosition).matches(END_PARENTHESIS)){
-								while (andRModifiedFormula.substring(searchPosition-1,searchPosition).matches(END_PARENTHESIS)){
-									searchPosition--;
-								}
-								
-								//Adds the splice after the first of these close parentheses
-								andRModifiedFormula = splicer.addSubformula(andRModifiedFormula, "^" + subformulaToAdd, searchPosition+1);
-							}
-						}
-					}
+					//Perform the AndR operation at this location in the andRModifiedFormula string
+					andRModifiedFormula = performAndEOperation(andRModifiedFormula, searchPosition);
 				}
 			}
-			
 			//Decrements until it has searched formula from end to beginning looking for &r operators
 			searchPosition--;
 		}
-		
-		//returns the formula with splices inserted in accordance with all &r operators contained in original formula
 		return andRModifiedFormula;
+	}
+	
+	private String performAndROperation(String formula, int searchPosition) {
+		
+		//Gets the portion of the formula after the &r operator
+		String subformulaToAdd = splicer.getSubformulaToAdd(formula, searchPosition + 1);
+		
+		//Removes the &r operator and the subformula it is splicing into the portion before the &r
+		formula = splicer.removeSubformula(formula, searchPosition, searchPosition + 1 + subformulaToAdd.length());
+		
+		
+		//Gets the beginning position of the portion of the formula that needs to be spliced into
+		int startPosition = splicer.getBeginSubformulaToSplicePosition(formula, searchPosition);
+		
+		//Searches the portion of formula that needs to be spliced into for the places the splice needs to be added
+		while (startPosition < searchPosition){
+			startPosition++;
+			
+			//Finds all the sets of close parentheses
+			if (formula.substring(startPosition,startPosition+1).matches(END_PARENTHESIS) && !formula.substring(startPosition-1,startPosition).matches("\\)")){
+				
+				//Adds the splice in front of these sets
+				formula = splicer.addSubformula(formula, "^" + subformulaToAdd, startPosition);
+			}
+			
+			//Finds all the "next" operators
+			else if (formula.substring(startPosition,startPosition+1).matches("X")){
+				
+				//Adds the splice in front of these operators
+				formula = splicer.addSubformula(formula, subformulaToAdd + "^", startPosition);
+			}
+		}
+		return formula;
+	}
+	
+	private String performAndEOperation(String formula, int searchPosition) {
+		
+		//Gets the portion of the formula after the &e operator
+		String subformulaToAdd = splicer.getSubformulaToAdd(formula, searchPosition + 1);
+		
+		//Removes the &e operator and the subformula it is splicing into the portion before the &e
+		formula = splicer.removeSubformula(formula, searchPosition, searchPosition + 1 + subformulaToAdd.length());
+		
+		//Gets the beginning position of the portion of the formula that needs to be spliced into
+		int startPosition = splicer.getBeginSubformulaToSplicePosition(formula, searchPosition);
+		
+		//Searches the portion of formula that needs to be spliced into for the places the splice needs to be added
+		while (searchPosition > startPosition+1){
+			searchPosition--;
+			
+			//Finds all the sets of multiple close parentheses
+			if (formula.substring(searchPosition-1,searchPosition).matches(END_PARENTHESIS)){
+				searchPosition--;
+				if (formula.substring(searchPosition-1,searchPosition).matches(END_PARENTHESIS)){
+					
+					//Finds the beginning of this set of parentheses
+					while (formula.substring(searchPosition-1,searchPosition).matches(END_PARENTHESIS)){
+						searchPosition--;
+					}
+					
+					//Adds the splice after the first of these close parentheses
+					formula = splicer.addSubformula(formula, "^" + subformulaToAdd, searchPosition+1);
+				}
+			}
+		}
+		return formula;	
 	}
 }
